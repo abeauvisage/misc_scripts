2bd183f68dc66f57c54b781317f9eb65c6d04fa5
Axel Beauvisage <axel.beauvisage@zenseact.com>
2021-01-19 22:57:23 +0100

    Solve pep8 and pylint errors
    Misc pep8 and pylint errors.

--- rename_images_by_date.py
+++ rename_images_by_date.py
@@ -3,9 +3,9 @@
 import os
 import argparse
 import sys
-import string
 import datetime
 
+import numpy as np
 from PIL import Image
 
 
@@ -20,7 +20,7 @@ def get_date_taken(path):
                 return value
     else:
         print("No metadata available for " + path)
-
+        return
 
 def get_cam_model(path):
     # get exif info
@@ -29,9 +29,12 @@ def get_cam_model(path):
         for tag, value in info.items():
             if tag == 272:  # if Model(272) tag found returns the corresponding value
                 return value.replace("\x00", "")
+    else:
+        print("No metadata available for " + path)
+        return
 
 
-def findDuplicates(file_dict):
+def find_duplicates(file_dict):
     final_list = []
     duplicates = 0
     for old_name, new_name in file_dict.items():
@@ -50,6 +53,11 @@ def findDuplicates(file_dict):
     return (duplicates, file_dict)
 
 
+###################
+# Argument parser #
+###################
+
+
 # Create the argument parser, that will display "rename" as program name
 # in output
 my_parser = argparse.ArgumentParser(
@@ -59,59 +67,70 @@ my_parser = argparse.ArgumentParser(
 # Add the arguments to parse in the CLI
 my_parser.add_argument(
     'dir',
-    metavar='pathTargetDirectory',
+    metavar='TARGET_DIR',
     type=str,
-    help='Input/Output directory -i.e. the target folder containing pics to rename')
+    help='Input/Output directory -i.e. the target folder containing pics '
+         'to rename')
 
 my_parser.add_argument(
-    '--undated',
+    '--no_date',
     action='store_true',
     help='If specified, pictures do not need date for renaming')
 my_parser.add_argument(
     '--cam',
     '-c',
-    metavar='cameraName',
+    metavar='CAMERA_NAME',
     type=str,
-    help='Camera for which a time delay should be applied (to be used with --time_delay)')
+    help='Camera for which a time delay should be applied '
+         '(to be used with --time_delay)')
 my_parser.add_argument(
     '--time_delay',
     '-t',
-    metavar='timeDelay',
+    metavar='DELAY',
     type=str,
-    help='Time delay to be applied to a Camera (to be used with --cam). Format "00:00:00" supported"')
+    help='Time delay to be applied to a Camera (to be used with --cam). '
+         'Format "00:00:00" supported".')
 my_parser.add_argument('--suffix', '-s',
-                       metavar='suffix',
+                       metavar='SUFFIX',
                        type=str,
                        default="",
                        help='Specify suffix for new renaming')
 my_parser.add_argument('--prefix', '-p',
-                       metavar='prefix',
+                       metavar='PREFIX',
                        type=str,
                        default="",
                        help='Specify prefix for new renaming')
 my_parser.add_argument('--verbose', '-v',
                        action='store_true',
                        help='If specified, plenty of printings')
-# Execute the parse_args() method
+
+# Extract arguments
 args = my_parser.parse_args()
 
-number = 1
 cam_name = args.cam
 time_delay = args.time_delay
 date_time_delay = datetime.timedelta()
-useDate = not args.undated
+use_date = not args.no_date
 suffix = args.suffix
 prefix = args.prefix
-isVerbose = args.verbose
+is_verbose = args.verbose
 
-if (time_delay and not cam_name) or (not time_delay and cam_name):
-    my_parser.error("--time_delay requires --cam_name. --cam_name requires --time_delay")
-if isVerbose:
-    print("Dated value: " + str(useDate))
+
+###############
+# Main script #
+###############
+
+
+# Initial checks
+if (time_delay and not cam_name) or (cam_name and not time_delay):
+    my_parser.error("--time_delay requires --cam_name and "
+                    "--cam_name requires --time_delay.")
+if is_verbose:
+    print("using dates? " + str(use_date))
 
 if time_delay:
     time_split = time_delay.split(":")
-    print("delay split: {}".format(time_split))
+    print("delay split: " + time_split)
     date_time_delay = datetime.timedelta(
         hours=int(
             time_split[0]), minutes=int(
@@ -120,25 +139,29 @@ if time_delay:
     print("time: " + str(date_time_delay))
 if cam_name:
     print("camera name: " + cam_name)
+
 dir = args.dir
 print("target directory: " + dir)
 files = {}
 
 # Find 'valid' file to rename
 for filename in sorted(os.listdir(dir)):
-    if isVerbose:
+    if is_verbose:
         print("File found: " + filename)
         # check for images in the directory
-    if filename.endswith(".jpg") or filename.endswith(".JPG") or filename.endswith(
-            ".jpeg") or filename.endswith(".JPEG") or filename.endswith(".PNG") or filename.endswith(".png"):
+    if filename.endswith((".jpg", ".JPG", ".jpeg", ".JPEG", ".PNG", ".png")):
         date = get_date_taken(dir + "/" + filename)
-        if date and useDate:
-            files[filename] = date
+        if use_date:
+            if date:
+                files[filename] = date
         else:
             files[filename] = filename
 
-if isVerbose:
+if is_verbose:
     print("Found {} file(s) to rename.".format(len(files)))
+
+# sort dict by date
+sorted_files = sorted(files.items(), key=lambda kv: kv[1])
 
 if len(files) == 0:
     print("No file found. Aborted.")
@@ -147,56 +170,58 @@ sorted_files = sorted(files.items(), key=lambda kv: kv[1])
 
 # Rename
 for filename, date in sorted_files:
-    if useDate:
+    main_name, ext = filename.split(".")
+
+    if use_date and date:
+        print(date)
         _date_, _time_ = date.split(" ")
         date_split = _date_.split(":")
         time_split = _time_.split(":")
         date_struct = datetime.datetime(
-            year=int(
-                date_split[0]), month=int(
-                date_split[1]), day=int(
-                date_split[2]), hour=int(
-                    time_split[0]), minute=int(
-                        time_split[1]), second=int(
-                            time_split[2]))
+            year=int(date_split[0]), month=int(date_split[1]),
+            day=int(date_split[2]), hour=int(time_split[0]),
+            minute=int(time_split[1]), second=int(time_split[2]))
 
-        if(cam_name == get_cam_model(dir + "/" + filename)):
+        if cam_name == get_cam_model(dir + "/" + filename):
             date_struct = date_struct + date_time_delay
         else:
             print((cam_name, get_cam_model(dir + "/" + filename)))
 
-        new_filename = str(date_struct.year) + "_" + str(date_struct.month).zfill(2) + "_" + str(date_struct.day).zfill(2) + "_" + str(
-            date_struct.hour).zfill(2) + "_" + str(date_struct.minute).zfill(2) + "_" + str(date_struct.second).zfill(2) + filename[filename.rfind("."):]
-        files[filename] = new_filename
+        new_name = \
+            str(date_struct.year) + "_" + \
+            str(date_struct.month).zfill(2) + "_" + \
+            str(date_struct.day).zfill(2) + "_" + \
+            str(date_struct.hour).zfill(2) + "_" + \
+            str(date_struct.minute).zfill(2) + "_" + \
+            str(date_struct.second).zfill(2)
+        files[filename] = new_name + "_" + ext
 
     if prefix:
-        previousname = files[filename]
-        files[filename] = prefix + "_" + previousname
-        if isVerbose:
+        files[filename] = prefix + "_" + main_name + "." + ext
+        if is_verbose:
             print("Prefix added: " + filename + " -> " + files[filename])
+
     if suffix:
-        previousname, ext = files[filename].split(".")
-        files[filename] = previousname + "_" + suffix + "." + ext
-        if isVerbose:
+        files[filename] = main_name + "_" + suffix + "." + ext
+        if is_verbose:
             print("Suffix added: " + filename + " -> " + files[filename])
 
 # look for duplicate
-nb_duplicates, files = findDuplicates(files)
+nb_duplicates, files = find_duplicates(files)
 
 for old_name, new_name in files.items():
     print("{} -> {}".format(old_name, new_name))
 
-# wait fro approval
-print(
-    "Would you like to continue the renaming (There is {} duplicate(s) over {} files)? [Y/N]".format(
-        nb_duplicates,
-        len(files)))
+# wait for approval
+print("Would you like to continue the renaming "
+      "(There are {} duplicate(s) over {} files)? [Y/N]".format(nb_duplicates,
+                                                                len(files)))
 x = input()
 
 #rename or exit
-if x == 'Y' or x == 'y':
+if x in ('Y', 'y'):
     for old_name, new_name in files.items():
         os.rename(dir + "/" + old_name, dir + "/" + new_name)
     print("{} files renamed!".format(len(files)))
 else:
-    exit()
+    sys.exit()
